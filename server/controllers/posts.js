191f44d9c7a689bb33b92b5b5ed1c230958c168a
Md. Nazmul Hasan <mdnazmulhasan@bracits.com>
2021-10-20 00:03:55 +0600

    get post and create post api has been created

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,27 @@
 const PostMessage = require('../models/postMessage')
 
-exports.getPost = (req, res) =>{
-    res.send('hello Express')
+exports.getPost = async (req, res) =>{
+    try{
+        const posts = await postMessage.find();
+
+        res.status(200).json(posts);
+
+    } catch(err){
+        res.status(404).json({ message: err.message })
+    }
 }
 
-exports.createPost = (req, res) =>{
-    res.send('create Express')
+exports.createPost = async(req, res) =>{
+
+    const post = req.body;
+    const newPost = new PostMessage(post);
+
+    try{
+
+        await newPost.save();
+        res.status(201).json(newPost);
+
+    } catch(err){
+        res.status(409).json({message: err.message})
+    }
 }
